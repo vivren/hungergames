6b7772ccb76b13cf038caca83126f486494982e2
Arthur <arthurchan2001@live.com>
2023-11-25 11:55:50 -0500

    nothing interesting here

--- server/main.py
+++ server/main.py
@@ -4,7 +4,14 @@ import mongoConnect
 
 app = FastAPI()
 
+@app.get("/restaurants")
+def get_restaurants(lat: float, long: float, radius: int, maxPrice: int):
+    # FOR TESTING
+    lat, long, radius, maxPrice = 43.51179542015055, -79.66749324203175, 10000, 3
+    excludedTypes = []
+    return(getRestaurants.main(lat, long, radius, maxPrice, excludedTypes))
 
+# FOR TESTING
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -13,10 +20,3 @@ def read_root():
 @app.get("/ping")
 def ping():
     return(mongoConnect.pingTest())
-
-@app.get("/restaurants")
-def get_restaurants(lat: float, long: float, radius: int, maxPrice: int):
-    # FOR TESTING
-    lat, long, radius, maxPrice = 43.51179542015055, -79.66749324203175, 10000, 3
-    excludedTypes = []
-    return(getRestaurants.main(lat, long, radius, maxPrice, excludedTypes))

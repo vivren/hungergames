339f45af94167a64b6a6dcaf0078c61492bf6ef4
Jacky <jackyliu1015@gmail.com>
2023-11-25 17:08:04 -0500

    ranking components

--- hungergames/src/components/RestaurantCard.tsx
+++ hungergames/src/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 // RestaurantCard.tsx
 import React from 'react';
 import styles from './RestaurantCard.module.css';
+import Image from 'next/image';
 
 interface RestaurantCardProps {
   restaurantName: string;
@@ -19,11 +20,11 @@ const RestaurantCard: React.FC<RestaurantCardProps> = ({
     <div className={styles.card}>
       <div className={styles.leftColumn}>
         <h2 className={styles.header}>{restaurantName}</h2>
-        <h3>{cuisine}</h3>
-        <h3>{distance}</h3>
+        <h3>🍽️{cuisine}</h3>
+        <h3>📍{distance} km away</h3>
       </div>
       <div className={styles.rightColumn}>
-        <img src={imageUrl} alt="Restaurant" className={styles.image} />
+        <Image src={imageUrl} alt="Restaurant" className={styles.image} width={150} height={150}/>
       </div>
     </div>
   );
--- hungergames/src/pages/Game.tsx
+++ hungergames/src/pages/Game.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import MobileFrame from '../components/MobileFrame';
+import RestaurantCard from '@/components/RestaurantCard';
 import './game.css'; // Import the CSS file for styling
+import Link from 'next/link';
 
 
 const Game: React.FC = () => {
   return (
-    <div className="centeredDiv">
-      <MobileFrame>
-
+    <div className="mainPage">
+            <MobileFrame>
+      <div className='header'>
+        <h1>SWIPE TO PICK!</h1>
+      </div>
+      <div className='cardContainer'>
+      <Link className='topCard' href="/Game">
+        <RestaurantCard 
+        restaurantName='Mcdonalds'
+        cuisine='American'
+        distance='5'
+        imageUrl='/fries.png'></RestaurantCard>
+      </Link>
+    
+      <Link className='bottomCard' href="/Game">
+        <RestaurantCard 
+        restaurantName='Mcdonalds'
+        cuisine='American'
+        distance='5'
+        imageUrl='/fries.png'></RestaurantCard>
+        </Link>
+        </div>
       </MobileFrame>
-    </div>
+      </div>
+
   );
 };
 

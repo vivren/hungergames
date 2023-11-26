28e0d637c88cc22023b39e9c12e38fbd9508a7c6
Arthur <arthurchan2001@live.com>
2023-11-25 21:17:10 -0500

    fix: fixed CORS

--- server/main.py
+++ server/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import getRestaurants
 import mongoConnect
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.post("/games")
 def create_game() -> int:
